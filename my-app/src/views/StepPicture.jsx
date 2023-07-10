e86beacf59ebaba0ee640c5a7b1052f43b938e71
Mateo <madime-1997@hotmail.com>
2023-07-10 16:56:38 -0500

    cambios tamaño stepImages

--- my-app/src/views/StepPicture.jsx
+++ my-app/src/views/StepPicture.jsx
@@ -7,27 +7,29 @@ const StepPicture = () => {
     return (
         <div className="blockFull StepPicture_component">
             <div className="divGlob">
-                <h2>16.09.2023</h2>
-                <div>
+                <div className="step1">
+                    <h2>16.09.2023</h2>
+                </div>
+                <div className="step2">
                     <h1>Camilo</h1>
                     <span>&</span>
                     <h1>Ana</h1>
                 </div>
                 <hr className="linea_espacio_blanca"/>
-                <div>
-                    <IconContext.Provider value={{size:"50px", color: '#c8c8c8'}}>
+                <div className="step3">
+                    <IconContext.Provider value={{size:"40px", color: '#c8c8c8'}}>
                         <div>
                             <RiDoubleQuotesL/>
                         </div>
                     </IconContext.Provider>
                     <h5>Cada familia tiene una historia, ¡Bienvenidos a la nuestra!</h5>
-                    <IconContext.Provider value={{size:"50px", color: '#c8c8c8'}}>
+                    <IconContext.Provider value={{size:"40px", color: '#c8c8c8'}}>
                         <div>
                             <RiDoubleQuotesR/>
                         </div>
                     </IconContext.Provider>
                 </div>
-                <div>
+                <div className="step4">
                     <img src={arrowDown} className="arrowDown"  alt="arrowDown" />
                 </div>
             </div>
